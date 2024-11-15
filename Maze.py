c8b526ba8c698b8d13db26ff74a75b8f8a33766b
alhusseain <husseainshalaby6@gmail.com>
2024-11-15 03:17:47 +0200

    Greedy Done, which means so is Astar

--- Maze.py
+++ Maze.py
@@ -10,36 +10,76 @@ VISITED=1
 X=0
 Y=1
 
+
+
 class Maze:
 
-    def __init__(self,width,height,GoalState=None,ActorState=(1,1)):
-        self.theMaze=maze(width,height)
+    def __init__(self,height,width,GoalState=None,ActorState=(1,1)):
+        self.theMaze=maze(height,width)
         self.theMaze.CreateMaze(loopPercent=50)
         self.Agent=Actor(self.theMaze,ActorState)
-        if GoalState==None:self.Goal=(width,height)
+        self.Size=(width,height)
+        if GoalState==None:self.Goal=(height,width)
         else: self.Goal=GoalState
 
         self.Grid=[[UNVISITED for i in range(width+1)] for j in range(height+1)]
+        
         self.Functions={
             'UCS':self.UCS,
-            'BFS':self.BFS
+            'BFS':self.BFS,
+            'GREEDY':self.Greedy
         }
 
-    def SearchFunction(self,Key,Cost=None):
-        self.Functions[Key.upper()](Cost)
+        self.HFunctions={
+            'MANHATTEN':self.Manhatten
 
-    def Start(self,Key,CostGrid = None):
+        }
+
+    def Path(self,Parent):
+            step=self.Goal
+            path=[]
+            while step is not None:
+                path.append(step)
+                step = Parent[step]
+            path.reverse()
+            return path
+
+    def SearchFunction(self,Key,Cost=None,HKey=None):
+        if Cost: self.Functions[Key.upper()](Cost)
+        elif HKey: self.Functions[Key.upper()](HKey)
+        else: self.Functions[Key.upper()]()
+
+    def HeuristicFunction(self,Key):
+        return self.HFunctions[Key.upper()]()
+    
+    def Start(self,Key,CostGrid = None,HKey=None):
         if(not self.Goal or not self.Agent.State):
             print("ERROR: Set all your States first :)")
             exit()
         self.Agent.theGUI.footprints=True
-        self.SearchFunction(Key,CostGrid)
+        if CostGrid:self.SearchFunction(Key,CostGrid)
+        elif HKey: self.SearchFunction(Key,HKey=HKey)
+        else:self.SearchFunction(Key)
         self.theMaze.run()
 
-#--------------------------------------------------------------------------
-# Enter your Search Algorithms here
+# Heuristic Functions Here
+    def Manhatten(self):
+            CostGrid=[]
+            for i in range(self.Size[X]+1):
+                row=[]
+                for j in range(self.Size[Y]+1):
+                    row.append(abs(self.Goal[Y]-j)+abs(self.Goal[X]-i))
+                CostGrid.append(row)
+            return CostGrid 
+
+
+
+
+# Search Algorithms here
 
     def UCS(self,CostGrid=None):
+        parent = {self.Agent.State: None}
+        path = []
         Queue=[(0,self.Agent.State)]
         print("Begin")
         if(not CostGrid):
@@ -53,17 +93,19 @@ class Maze:
             for state in self.Agent.Actions():
                 if CostGrid[state[X]][state[Y]]+self.Grid[CurrState[X]][CurrState[Y]]>=self.Grid[state[X]][state[Y]]: continue
                 self.Grid[state[X]][state[Y]]=CostGrid[state[X]][state[Y]]+self.Grid[CurrState[X]][CurrState[Y]]
+                parent[state] = CurrState
                 heappush(Queue,(self.Grid[state[X]][state[Y]],state))
         if(self.Grid[self.Goal[X]][self.Goal[Y]]==UNVISITED):print("No Path to Goal :(")
         else:
             print("Path cost is ", self.Grid[self.Goal[X]][self.Goal[Y]])
+            print("Path is ", self.Path(parent))
+
             
     def BFS(self, CostGrid=None):
         Queue=deque()
         Queue.append(self.Agent.State)
         self.Grid[self.Agent.State[X]][self.Agent.State[Y]]=0
         parent = {self.Agent.State: None}
-        path = []
         explored = []
 
         while Queue:
@@ -82,13 +124,16 @@ class Maze:
             print("No Path to Goal :(")
         else:
             print("Path Length is ", self.Grid[self.Goal[X]][self.Goal[Y]])
-            step = self.Goal
-            while step is not None:
-                path.append(step)
-                step = parent[step]
-            path.reverse()
-            print("Path is ", path)
             print("Explored nodes are ", explored)
+            print("Path is ",self.Path(parent))
+    
+    def Greedy(self,HKey):
+        CostGrid=self.HeuristicFunction(HKey)
+        print(CostGrid)
+        self.SearchFunction('UCS',CostGrid)
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Actor import *
 from Maze import *
 
-env=Maze(4,4,(2,1))
+env=Maze(10,10,(5,9))
 
-env.Start('UCS',[[i for i in range(5)]]*5)
+env.Start('greedy',HKey='Manhatten')
